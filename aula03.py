326bb50f54ca23645f505e9d1563cdfba938b0f4
Rafaela dos Anjos <163443768+13rafaela@users.noreply.github.com>
2026-04-28 21:26:51 -0300

    Add files via upload

---
+++ aula03.py
@@ -0,0 +1,10 @@
+a = int(input("Digite um numero: "))
+b = int(input("Digite outro numero:  "))
+soma = a + b
+divisao = a // b
+mult = a * b
+sub = a - b
+print(f"Soma: {soma}")
+print(f"Divisão: {divisao}")
+print(f"multiplicação: {mult}")
+print(f"subtração: {sub}")
---
+++ exercicio_01.py
@@ -0,0 +1,4 @@
+nome = input("Digite seu nome:")
+tel = input("Digite seu celular:")
+email = input("Digite seu email:")
+print(f"seu nome é:{nome} Seu contato:{tel} seu e mail: {email}")
