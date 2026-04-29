66a4b07102980897c9fb8a1c19b179392eac7f64
Rafaela dos Anjos <163443768+13rafaela@users.noreply.github.com>
2026-04-28 21:37:13 -0300

    Add files via upload

---
+++ exercicio02.py
@@ -0,0 +1,5 @@
+num1 = float(input("Digite um numero: "))
+num2 = float(input("Digite outro numero: "))
+num3 = float(input("Digite o ultimo numero: "))
+m = (num1 + num2 + num3) // 2
+print(f"A média é: {m:.2f}") 
