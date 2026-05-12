bd3786dbb007f3b2833189b0ad4d44fd670e5d7c
Rafaela dos Anjos <163443768+13rafaela@users.noreply.github.com>
2026-05-12 22:56:55 +0000

    rgr

---
+++ funcao.py
@@ -0,0 +1,26 @@
+#def comprimentar():
+    #print("ola, bem vinda!")
+
+
+#comprimentar()
+#def somar(a,b):
+    #print(a+b) 
+#resultado = somar(10,5)
+#print(resultado)
+#nome_global = "Ana"
+#ef teste():
+    #nome_local = "Bia"
+    #print(nome_local)
+
+#teste()
+
+#def verificar_idade(nome, idade):
+    #if idade >=18:
+        #return f"{nome} é maior de idade "
+    #else:
+        #return f"{nome} é menor de idade "
+#print(verificar_idade("Nathalya,",17))
+import math
+import random
+num = math.sqrt(25)
+print(num)
