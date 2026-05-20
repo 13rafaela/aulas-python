8ad851717927bd201e36c1887965c2d9b4c229eb
Rafaela dos Anjos <163443768+13rafaela@users.noreply.github.com>
2026-05-20 20:09:50 -0300

    Update aula1.py

--- aula1.py
+++ aula1.py
@@ -9,18 +9,5 @@ bolo2 = Bolo("morango", "chantily")
 bolo1.assar()
 bolo2.assar()
 
-
-class Animal:
-    def __init__(self, nome,especie,idade):
-        self.nome = nome
-        self.especie = especie
-        self.idade = idade
-    def mostrar(self):
-        print(f"olá me chamo {self.nome} e sou um {self.especie}, e tenho {self.idade} aninhos")
-
-animal1 = Animal("Lulu","cachorro","6")
-animal2 = Animal("Betty","tartaruga","3")
-animal3 = Animal("chris","lobo","4")
-        
     
     
